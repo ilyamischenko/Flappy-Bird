5c58f6cf34697182ebdc8bddfbfa127dc27a4da1
Ilya <ilyamishencko@gmail.com>
2022-05-30 16:28:35 +0300

    Fix

--- js/script.js
+++ js/script.js
@@ -20,24 +20,24 @@ let birdUp = new Image();
 let birdDown = new Image();
 let birdMid = new Image();
 
-medalOne.src = "/imgBird/medal_1.png";
-medalTo.src = "/imgBird/medal_2.png";
-medalThree.src = "/imgBird/medal_3.png";
-medalFo.src = "/imgBird/medal_4.png";
-bird.src = "/imgBird/bluebird-midflap.png";
-bg.src = "/img/background.png";
-fg.src = "/img/floor.png";
-p1.src = "/img/pipeUp.png";
-p2.src = "/img/pipeBottom.png";
-sprite.src = "/img/sprite.png ";
-getReady.src = "/imgBird/getReady.png";
-gameOver.src = "/imgBird/gameOver.png";
-scoreMedal.src = "/imgBird/scoreboard.png";
-restart.src = "/img/restart.png";
+medalOne.src = "imgBird/medal_1.png";
+medalTo.src = "imgBird/medal_2.png";
+medalThree.src = "imgBird/medal_3.png";
+medalFo.src = "imgBird/medal_4.png";
+bird.src = "imgBird/bluebird-midflap.png";
+bg.src = "img/background.png";
+fg.src = "img/floor.png";
+p1.src = "img/pipeUp.png";
+p2.src = "img/pipeBottom.png";
+sprite.src = "img/sprite.png ";
+getReady.src = "imgBird/getReady.png";
+gameOver.src = "imgBird/gameOver.png";
+scoreMedal.src = "imgBird/scoreboard.png";
+restart.src = "img/restart.png";
 
-birdUp.src = "/imgBird/bluebird-upflap.png";
-birdDown.src = "/imgBird/bluebird-downflap.png";
-birdMid.src = "/imgBird/bluebird-midflap.png";
+birdUp.src = "imgBird/bluebird-upflap.png";
+birdDown.src = "imgBird/bluebird-downflap.png";
+birdMid.src = "imgBird/bluebird-midflap.png";
 
 let gameRestart = false;
 let isGameActive = true;
@@ -58,7 +58,7 @@ let score_sound = new Audio();
 fly_sound.src = "audio/fly.mp3";
 score_sound.src = "audio/score.mp3";
 
-bird.src = "/imgBird/bluebird-upflap.png";
+bird.src = "imgBird/bluebird-upflap.png";
 birdY -= 30;
 fly_sound.play();
 
@@ -85,7 +85,7 @@ const setBirdDirectionUp = () => {
 const setBirdDirectionDownDebounce = debounce(() => setBirdDirectionDown());
 
 function moveUp() {
-  bird.src = "/imgBird/bluebird-upflap.png";
+  bird.src = "imgBird/bluebird-upflap.png";
   setBirdDirectionUp();
   setBirdDirectionDownDebounce();
   fly_sound.play();
@@ -148,6 +148,7 @@ function draw() {
       }
 
       cvs.addEventListener("click", gameRestart);
+      cvs.addEventListener("keydown", gameRestart);
 
       if (score < 2) {
         cxt.drawImage(medalOne, 55, 148);
@@ -168,7 +169,7 @@ function draw() {
   cxt.drawImage(bird, birdX, birdY);
   if (birdDirection === "down") {
     birdY += gravity;
-    bird.src = "/imgBird/bluebird-downflap.png";
+    bird.src = "imgBird/bluebird-downflap.png";
   } else {
     birdY -= gravity;
   }
